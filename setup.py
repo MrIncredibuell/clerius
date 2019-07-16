da3efa1a9504823179406ed9c8917a1d01b5b36a
Joseph <jlrbuellv@gmail.com>
2019-07-15 21:26:38 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clericus",
-    version="0.0.3a7",
+    version="0.0.3a8",
     author="Joseph L Buell V",
     author_email="jlrbuellv@gmail.com",
     description=
